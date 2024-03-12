fe624d48f21262dd35184ed47fda475aaa44cdf0
Erik Lattimore <elatt@users.noreply.github.com>
2024-03-11 21:03:20 -0400

    Add support for openai API

--- custom-env/start_server.sh
+++ custom-env/start_server.sh
@@ -26,6 +26,11 @@ export FLASK_RUN_PORT=8080
 export FLASK_RUN_HOST="0.0.0.0"
 export FLASK_APP=${CODE_DIR}/hello.py
 export MODEL_DIR="${CODE_DIR}/model-store/"
+export MODEL_NAME=generic-llm
+
+export NEMO_PORT=9998
+export OPENAI_PORT=9999
+export HEALTH_PORT=8081
 
 nohup flask run > log.txt 2>&1 &
 
@@ -40,8 +45,9 @@ deactivate
 echo
 echo "Starting NeMo Inference Microservice..."
 echo
-exec nemollm_inference_ms --model llm \
-    --health_port=8081 \
-    --openai_port="9999" \
-    --nemo_port="9998" \
+exec nemollm_inference_ms --model $MODEL_NAME \
+    --log_level=info \
+    --health_port=$HEALTH_PORT \
+    --openai_port=$OPENAI_PORT \
+    --nemo_port=$NEMO_PORT \
     --num_gpus=$(nvidia-smi -L | wc -l)
